065c99dce2e3bf1f2ab57ce41a11f09ae8de091d
Guillaume Le Dourner <guillaume.le-dourner@etudiant.univ-rennes1.fr>
2022-03-10 15:20:12 +0100

    Push date
    Co-authored-by: Alexys GUERIN <asyyy@users.noreply.github.com>

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -7,5 +7,86 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+@Test 
+public void testEmpty(){
+    assertTrue(isBalanced(""));
+}
+@Test 
+public void testOneOpenParenthesis(){
+    assertFalse(isBalanced("("));
+}
+@Test 
+public void testOneClosedPrenthesis(){
+    assertFalse(isBalanced(")"));
+}
+@Test 
+public void testOneOpenCurlyBracket(){
+    assertFalse(isBalanced("{"));
+}
+@Test 
+public void testOneClosedCurlyBracket(){
+    assertFalse(isBalanced("}"));
+}
+@Test 
+public void testOneOpenBracket(){
+    assertFalse(isBalanced("["));
+}
+@Test 
+public void testOneClosedBracket(){
+    assertFalse(isBalanced("]"));
+}
+@Test
+public void testParenthesis(){
+    assertTrue(isBalanced("()"));
+}
+@Test
+public void testBracket(){
+    assertTrue(isBalanced("[]"));
+}
+@Test
+public void testCurlyBracket(){
+    assertTrue(isBalanced("{}"));
+}
+@Test
+public void testMultipleParenthesis(){
+    assertTrue(isBalanced("()()(())((()))"));
+}
+@Test
+public void testMultipleBracket(){
+    assertTrue(isBalanced("[[[]][]][][[]]"));
+}
+@Test
+public void testMultipleCurlyBracket(){
+    assertTrue(isBalanced("{{{}}}{}{{}}{}"));
+}
+@Test
+public void testMultipleParenthesisOneNotClosed(){
+    assertFalse(isBalanced("()()(()()((()))"));
+}
+@Test
+public void testMultipleParenthesisOneNotOpened(){
+    assertFalse(isBalanced("()()())((()))"));
+}
+@Test
+public void testMultipleBracketOneNotClosed(){
+    assertFalse(isBalanced("[[[]][][[]]"));
+}
+@Test
+public void testMultipleBracketOneNotOpened(){
+    assertFalse(isBalanced("[[]][[]][][[]]]"));
+}
+@Test
+public void testMultipleCurlyBracketOneNotClosed(){
+    assertFalse(isBalanced("{{{{}}}{}"));
+}
+@Test
+public void testMultiplePCurlyBracketOneNotOpened(){
+    assertFalse(isBalanced("{{}{}}{{}}}"));
+}
+
+@Test
+public void testIncorrect(){
+    assertFalse(isBalanced("([)]"));
+}
 
 }
--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -1,17 +1,84 @@
 package fr.istic.vv;
 
+import java.io.IOException;
+import java.time.YearMonth;
+import java.util.Calendar;
+
 class Date implements Comparable<Date> {
 
-    public Date(int day, int month, int year) { }
+    private int day;
+    private int month;
+    private int year;
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+     }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public static boolean isValidDate(int day, int month, int year) { 
+        if(year > 0){           
+            if(month > 0 && month <= 12){
+                YearMonth y = YearMonth.of(year,month);
+                if(day > 0 && day <= y.lengthOfMonth()){
+                    return true;
+                }
+            }
+        }
+        return false;
+     }
 
-    public Date nextDate() { return null; }
+    public static boolean isLeapYear(int year) { 
+        return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+     }
 
-    public Date previousDate() { return null; }
+    public Date nextDate() { 
+        YearMonth y = YearMonth.of(this.year,this.month);
+        if(day > 0 && day < y.lengthOfMonth()){
+            return new Date(this.day + 1, this.month, this.year);
+        }
+        if(day == y.lengthOfMonth() && this.month < 12){
+            return new Date(1, this.month+1, this.year);
+        }
+        if(day == y.lengthOfMonth() && this.month == 12){
+            return new Date(1, 1, this.year+1);
+        }
+        return null;
+     }
 
-    public int compareTo(Date other) { return 0; }
+    public Date previousDate() { 
+        YearMonth y = YearMonth.of(this.year,this.month);
+        if(day > 0 && day < y.lengthOfMonth()){
+            return new Date(this.day - 1, this.month, this.year);
+        }
+        if(day == 1 && this.month < 12){
+            YearMonth y2 = YearMonth.of(this.year,this.month-1);
+            return new Date(y2.lengthOfMonth(), this.month-1, this.year);
+        }
+        if(day == 1 && this.month == 1){
+            return new Date(31, 12, this.year-1);
+        }
+        return null;
+    }
+
+    public static void main(String args[]) throws IOException {
+        System.out.println(isLeapYear(2022));
+    }
+
+    public int compareTo(Date other) {
+        if(other != null) {
+            if(this.day == other.day && this.month == other.month && this.year==other.year){
+                return 0;
+            }     
+            if(this.year > other.year || (this.year == other.year && this.month > other.month) ||
+                (this.year == other.year && this.month == other.month && this.day > other.day)  ){
+                return 1;
+            }else{
+                return -1;   
+            }  
+        }
+        
+        throw new NullPointerException("input value is null");
+    }
 
 }
